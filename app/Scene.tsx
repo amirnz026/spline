d1113082f4d1d8638445a15923337b7bc9161bd8
amirnz026 <czhablog@gmail.com>
2023-08-08 22:41:55 +0330

    Test

--- app/Scene.tsx
+++ app/Scene.tsx
@@ -2,12 +2,7 @@
 import Spline from "@splinetool/react-spline";
 
 export default function Scene() {
-  return (
-    <Spline
-      className="h-screen"
-      scene="https://prod.spline.design/Iu-v2AJhUXO00VM2/scene.splinecode"
-    />
-  );
+  return <Spline className="h-screen" scene="/scene.splinecode" />;
 }
 // test;
 // test 2

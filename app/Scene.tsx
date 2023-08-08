893799c39cc940ccdbb8bff647616c31ccea093d
amirnz026 <czhablog@gmail.com>
2023-08-08 22:41:41 +0330

    Change

--- app/Scene.tsx
+++ app/Scene.tsx
@@ -2,6 +2,12 @@
 import Spline from "@splinetool/react-spline";
 
 export default function Scene() {
-  return <Spline className="h-screen" scene="/scene.splinecode" />;
+  return (
+    <Spline
+      className="h-screen"
+      scene="https://prod.spline.design/Iu-v2AJhUXO00VM2/scene.splinecode"
+    />
+  );
 }
 // test;
+// test 2
